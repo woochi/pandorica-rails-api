b77befe2b15a0646b9548972831c5e67ca24b389
Mikko Kivelä <mikko@smartly.io>
2017-07-28 16:55:22 +0300

    Fix signup faction

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,3 +1,10 @@
 class ApiController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:faction_id])
+  end
 end
