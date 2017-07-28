63795efe37e0666bff3a788b370729579520e3dc
Mikko Kivelä <mikko@smartly.io>
2017-07-28 22:51:10 +0300

    Batch export action

--- app/admin/code.rb
+++ app/admin/code.rb
@@ -30,6 +30,57 @@ ActiveAdmin.register Code do
     link_to 'Export Codes', export_admin_codes_path, target: "_blank"
   end
 
+  batch_action :export do |ids|
+    zipfile_name = "codes.zip"
+    zipfile_path = "#{Rails.root}/tmp/#{zipfile_name}"
+    image_names = []
+
+    File.delete(zipfile_path) if File.exist?(zipfile_path)
+
+    Code.find(ids).each do |code|
+      canvas = Magick::ImageList.new
+      qr = RQRCode::QRCode.new(
+        "https://app.ropecon.fi/codes?value=#{code.value}",
+        :size => 8,
+        :level => :h
+      ).as_png(size: 480).to_s
+      qr_image = Magick::Image.from_blob(qr)[0]
+      canvas << qr_image
+
+      [
+        'Scan the QR code or fill\n the code below in the app:',
+        code.value.upcase,
+        'Join the fight at app.ropecon.fi'
+      ].each_with_index do |value, index|
+        text_image = Magick::Image.new(480, 100)
+        text = Magick::Draw.new
+        text.font_family = 'helvetica'
+        text.pointsize = index === 1 ? 52 : 28
+        text.gravity = Magick::CenterGravity
+        text.annotate(text_image, 0,0,2,2, value) {
+          self.fill = 'gray0'
+        }
+        canvas << text_image
+      end
+
+      image_name = "#{code.value}.png"
+      image_names << image_name
+      canvas.append(true).write("#{Rails.root}/tmp/#{image_name}")
+    end
+
+    Zip::File.open(zipfile_path, Zip::File::CREATE) do |zipfile|
+      image_names.each do |image_name|
+        # Two arguments:
+        # - The name of the file as it will appear in the archive
+        # - The original file, including the path to find it
+        zipfile.add(image_name, "#{Rails.root}/tmp/#{image_name}")
+      end
+    end
+    zip_data = File.read(zipfile_path)
+
+    send_data(zip_data, :type => 'application/zip', :filename => zipfile_name)
+  end
+
   collection_action :export do
     zipfile_name = "codes.zip"
     zipfile_path = "#{Rails.root}/tmp/#{zipfile_name}"
